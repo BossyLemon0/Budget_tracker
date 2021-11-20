bf7d53ef36bdbd34b0763ce3f101645ca13a1bad
Omar D. Ramirez <omardramirez2002@gmail.com>
2021-11-20 14:52:36 -0600

    This commit adds the creation of the offline database and a fix to the service worker.

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,1 +1,52 @@
-console.log('jello');
+const FILES_TO_CACHE = [
+    '/',
+    '/index.html',
+    '/offlineDb.js',
+    '/styles.css',
+    '/index.js',
+    '/icons/icon-192x192.png',
+    '/icons/icon-512x512.png'
+];
+  
+const PRECACHE = 'precache-budget';
+const RUNTIME = 'runtime-budget';
+  
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+      caches
+        .open(PRECACHE)
+        .then((cache) => cache.addAll(FILES_TO_CACHE))
+        .then(self.skipWaiting())
+    );
+});
+  
+self.addEventListener("fetch", function(evt) {
+  if (evt.request.url.includes("/api/")) {
+    evt.respondWith(
+      caches.open(RUNTIME).then(cache => {
+        return fetch(evt.request)
+          .then(response => {
+            // If the response was good, clone it and store it in the cache.
+            if (response.status === 200) {
+              cache.put(evt.request.url, response.clone());
+            }
+
+            return response;
+          })
+          .catch(err => {
+            // Network request failed, try to get it from the cache.
+            return cache.match(evt.request);
+          });
+      }).catch(err => console.log(err))
+    );
+
+    return;
+  }
+
+  // if the request is not for the API, serve static assets using "offline-first" approach.
+  evt.respondWith(
+    caches.match(evt.request).then(function(response) {
+      return response || fetch(evt.request);
+    })
+  );
+});
